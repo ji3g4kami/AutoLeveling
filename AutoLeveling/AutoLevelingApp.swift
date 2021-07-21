793452b054121594f49865d070feafa21071f659
ji3g4kami <ji3g4kami@gmail.com>
2021-07-22 00:18:30 +0100

    Initial Commit

---
+++ AutoLeveling/AutoLevelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoLevelingApp.swift
+//  AutoLeveling
+//
+//  Created by 登秝吳 on 22/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct AutoLevelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoLeveling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AutoLeveling
+//
+//  Created by 登秝吳 on 22/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
